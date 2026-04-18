725638a15530228cc65afa620dc8aa1c549eeba5
Gilles Darold <gilles@darold.net>
2026-04-18 15:17:25 +0300

    Remove drop of NOTIFY triggers from update file.

--- updates/pg_dbms_job--1.5.0--2.0.0.sql
+++ updates/pg_dbms_job--1.5.0--2.0.0.sql
@@ -1,8 +1,2 @@
 -- Column all_scheduler_job_run_details.run_duration have been change to interval datatype
 ALTER TABLE dbms_job.all_scheduler_job_run_details ALTER COLUMN run_duration TYPE interval USING (run_duration||' seconds')::interval;
--- NOTIFY is no more necessay with the background worker
-DROP TRIGGER dbms_job_scheduled_notify_trg;
-DROP FUNCTION dbms_job.job_scheduled_notify();
-DROP TRIGGER dbms_job_async_notify_trg;
-DROP FUNCTION dbms_job.job_async_notify();
-
